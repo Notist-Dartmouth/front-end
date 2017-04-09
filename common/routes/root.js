f2625c4a5d501db26292a99317555bf52eaeba70
Peter Loomis <loomispeter@gmail.com>
2017-04-09 15:03:23 -0400

    login page styling

--- common/routes/root.js
+++ common/routes/root.js
@@ -1,26 +1,29 @@
 import App from '../components/App';
 import Home from './Home';
+import loginRoute from './Login';
 
 // polyfill webpack require.ensure
 if (typeof require.ensure !== 'function') require.ensure = (d, c) => c(require);
 
 export default function createRoutes(store) {
-  return {
-    path: '/',
-    component: App,
-    getChildRoutes(location, cb) {
-      require.ensure([], (require) => {
-        cb(null, [
-          require('./PostList').default(store), // no need to modify store, no reducer
-          require('./Post').default(store), // add async reducer
-          require('./Login').default,
-          require('./NotFound').default,
-        ]);
-      });
-    },
+  return [
+    loginRoute,
+    {
+      path: '/',
+      component: App,
+      getChildRoutes(location, cb) {
+        require.ensure([], (require) => {
+          cb(null, [
+            require('./PostList').default(store), // no need to modify store, no reducer
+            require('./Post').default(store), // add async reducer
+            require('./NotFound').default,
+          ]);
+        });
+      },
 
-    indexRoute: {
-      component: Home,
+      indexRoute: {
+        component: Home,
+      },
     },
-  };
+  ];
 }
