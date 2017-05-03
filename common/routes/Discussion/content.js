3e97faa73b71aeaa3048cc588402c743afc4e5ff
Mark Erwin <spserwinm@gmail.com>
2017-05-03 15:41:00 -0400

    Got rid of unused files, disabled psot button if user enters nothing in the textbox, general code clean up and made it so that the annotation appears as a first level comment box

--- common/routes/Discussion/content.js
+++ common/routes/Discussion/content.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import Media from 'react-media';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RightSideBar from './components/RightSideBar';
-import MyCard from './components/MyCard';
 import Comments from './components/Comments';
 import { loadDiscussion } from './actions';
 
@@ -31,7 +30,6 @@ class App extends Component {
             {this.props.annotations.filter(a => a.article === this.props.location.state.articleId)
               .map(a =>
                 <div key={a._id} >
-                  <MyCard annotation={a} />
                   <Comments
                     articleURI={this.props.location.state.articleURI}
                     replies={a}
