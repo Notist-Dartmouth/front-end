d923a8b716e0db97ab530d98c5dfafae98a3c0a2
Byrne Hollander <bhollander823@gmail.com>
2017-05-07 18:37:36 -0400

    adding new relic software monitoring to production front end

---
+++ newrelic.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['notist-f'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '5096d8886c652a8170354101e06c3ed1974cfe0d',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info',
+  },
+};
