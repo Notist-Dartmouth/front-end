9541ac277a52b79e8b214c629940c202d046d749
Mark Erwin <spserwinm@gmail.com>
2017-05-11 18:59:49 -0400

    Author error fix

--- common/routes/PostList/components/ArticleItem.js
+++ common/routes/PostList/components/ArticleItem.js
@@ -10,7 +10,7 @@ const ArticleItem = props => (
     subtitle={props.annotations.length > 0 ? props.annotations[0].articleText : 'article text'}
     annotationContent={props.annotations.length > 0 ? props.annotations[0].text : 'text'}
     image={props.imageURL}
-    username={props.annotations[0] ? props.annotations[0].author : 'username'}
+    username={props.annotations[0] ? props.annotations[0].author.name : 'username'}
     timeSince={props.annotations[0] ? props.annotations[0].createDate : String(Date.now())}
     numUsers={0}
     numAnnotations={props.annotations.length}
