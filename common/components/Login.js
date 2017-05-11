26b60c57813ba4223f0990e43d8af379a56705d5
weazel02 <weazel02@gmail.com>
2017-05-10 20:59:07 -0400

    Changes to login page

--- common/components/Login.js
+++ common/components/Login.js
@@ -3,36 +3,91 @@ import { StyleSheet, css } from 'aphrodite';
 import config from '../../server/config';
 
 const styles = StyleSheet.create({
+  navBar: {
+    position: 'fixed',
+    width: '100%',
+    height: '8%',
+    backgroundColor: '#ea7b30',
+    top: '0',
+  },
   root: {
     position: 'absolute',
-    top: '20%',
+    height: '100%',
     width: '100%',
+    backgroundColor: '#44808C',
+    top: '0',
   },
   loginForm: {
+    backgroundColor: '#FFFFFF',
     position: 'relative',
     width: '50%',
+    height: '60%',
     textAlign: 'center',
-    margin: '0 auto',
+    margin: '10% auto',
   },
   header: {
-    fontSize: 80,
+    fontSize: 60,
     lineHeight: 1,
+    postion: 'relative',
+    marginTop: '20px',
   },
   button: {
-    width: '25%',
+    width: '40%',
+    height: 'auto',
     display: 'block',
     margin: '0 auto',
     top: '20px',
+    textAlign: 'center',
+  },
+  button1: {
+    width: '40%',
+    height: 'auto',
+    display: 'block',
+    margin: '50px auto',
+    top: '20px',
+    textAlign: 'center',
+  },
+  titleText: {
+    color: '#FFFFFF',
+    fontSize: '40px',
+    margin: '10px',
+    position: 'relative',
+    display: 'inline',
+    top: '15%',
+  },
+  homeNav: {
+    color: '#FFFFFF',
+    float: 'right',
+    margin: '25px',
+    fontSize: '20px',
+    top: '0',
+    position: 'relative',
+    display: 'inline',
+    textDecoration: 'none',
+  },
+  loginText: {
+    marginTop: '5%',
+    fontSize: '30px',
   },
 });
 
 const Login = props => (
   <div className={css(styles.root)}>
+    <div className={css(styles.navBar)}>
+      <p className={css(styles.titleText)}>
+        Notist
+      </p>
+      <a className={css(styles.homeNav)}href={'http://notist.io'}>Home
+      </a>
+    </div>
     <div className={css(styles.loginForm)}>
-      <h1 className={css(styles.header)}>Notist</h1>
+      <h1 className={css(styles.header)}>Login</h1>
+      <p className={css(styles.loginText)}> Not a member yet? Login
+          <a href={'http://notist.io/signup'}> here </a>
+      </p>
       <br />
       <a href={`${config.apiHost}/login/facebook`}
-        className={`btn btn-block btn-social btn-lg btn-facebook ${css(styles.button)}`}
+        className={`btn btn-block btn-social btn-lg btn-facebook ${css(styles.button1)}`}
       >
         <i className="fa fa-facebook" /> Sign in with Facebook
       </a>
