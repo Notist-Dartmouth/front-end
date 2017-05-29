a1ca914ecd67cd3d7e637ba2037fb2c8683ebe35
Mark Erwin <spserwinm@gmail.com>
2017-05-29 14:42:07 -0400

    Improved annotation cards with time since on profile view

--- common/routes/Profile/components/annotation.js
+++ common/routes/Profile/components/annotation.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { Card, CardHeader } from 'material-ui/Card';
-import { styles, muiTheme } from '../../Discussion/styles/styles';
+import { StyleSheet, css } from 'aphrodite';
+import { yellow200 } from 'material-ui/styles/colors';
+import { Card, CardText } from 'material-ui/Card';
+import moment from 'moment';
+import { muiTheme } from '../../Discussion/styles/styles';
+
 
 // Pass an annotation prop in here with fields title, subtitle, and expandableText
+
+const styles = StyleSheet.create({
+  authorLine: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  authorAndTime: {
+    display: 'flex',
+  },
+  authorName: {
+    paddingRight: 10,
+  },
+});
 
 class Annotation extends Component {
 
@@ -13,19 +30,54 @@ class Annotation extends Component {
   }
 
   render() {
-    console.log('EXECUTING RENDER OF AN ANNOTATION');
-    console.log(`Hi, the value inside annotation is: ${this.annotation}`);
-    console.log(this.props.annotation);
     return (
+      <div style={{
+        paddingTop: 2,
+        paddingRight: 10,
+        paddingLeft: 10,
+      }}
+      >
+        <MuiThemeProvider muiTheme={muiTheme}>
+          <Card>
+            <CardText expandable={false}>
 
-      <MuiThemeProvider muiTheme={muiTheme}>
-        <Card>
-          <CardHeader style={styles.cardHeader}
-            title={this.props.annotation.parent === null ? this.props.annotation.articleText : ''}
-            subtitle={<a href={`http://localhost:5000/discussion/${this.props.annotation.article}`}>{this.props.annotation.text}</a>}
-          />
-        </Card>
-      </MuiThemeProvider>
+              {/* <Avatar  */}
+              {/*  src={'https://i.imgur.com/9zgiD0u.jpg'} */}
+              {/*    size={35} */}
+              {/*  /> */}
+
+              <div className={css(styles.authorLine)}>
+                <div className={css(styles.authorAndTime)}>
+                  <div className={css(styles.authorName)}>
+                    <b>{this.props.annotation.creatorName}</b>
+                  </div>
+                  {moment(this.props.annotation.createDate).fromNow()}
+                </div>
+              </div>
+              {/* {madeThisComment ?
+                <IconMenu
+                  style={{ marginLeft: '96%', marginRight: '25px' }}
+                  iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+                  anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+                  targetOrigin={{horizontal: 'right', vertical: 'top'}}
+                >
+                  <MenuItem primaryText="Edit" onClick={this.handleEditReply}/>
+                  <MenuItem primaryText="Delete" onClick={this.handleDeleteReply} />
+                </IconMenu> : ''}
+              <br /> <br /> */}
+
+              <div style={{
+                display: 'inline',
+                fontStyle: 'italic',
+                backgroundColor: yellow200,
+              }}
+              >
+                {(this.props.id === 0) ? this.props.articleText : ''}
+              </div>
+            </CardText>
+          </Card>
+        </MuiThemeProvider>
+      </div>
     );
   }
 }
