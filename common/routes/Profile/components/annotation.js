5db5f858393a8c03b7f58a1d2365e15629f01d76
Mark Erwin <spserwinm@gmail.com>
2017-05-29 15:04:36 -0400

    Further improvements to annotation card-- added link

--- common/routes/Profile/components/annotation.js
+++ common/routes/Profile/components/annotation.js
@@ -5,7 +5,7 @@ import { yellow200 } from 'material-ui/styles/colors';
 import { Card, CardText } from 'material-ui/Card';
 import moment from 'moment';
 import { muiTheme } from '../../Discussion/styles/styles';
-
+import config from '../../../../server/config';
 
 // Pass an annotation prop in here with fields title, subtitle, and expandableText
 
@@ -30,6 +30,9 @@ class Annotation extends Component {
   }
 
   render() {
+    console.log('Annotation: ');
+    console.log(this.props.annotation);
+
     return (
       <div style={{
         paddingTop: 2,
@@ -40,12 +43,6 @@ class Annotation extends Component {
         <MuiThemeProvider muiTheme={muiTheme}>
           <Card>
             <CardText expandable={false}>
-
-              {/* <Avatar  */}
-              {/*  src={'https://i.imgur.com/9zgiD0u.jpg'} */}
-              {/*    size={35} */}
-              {/*  /> */}
-
               <div className={css(styles.authorLine)}>
                 <div className={css(styles.authorAndTime)}>
                   <div className={css(styles.authorName)}>
@@ -54,26 +51,15 @@ class Annotation extends Component {
                   {moment(this.props.annotation.createDate).fromNow()}
                 </div>
               </div>
-              {/* {madeThisComment ?
-                <IconMenu
-                  style={{ marginLeft: '96%', marginRight: '25px' }}
-                  iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
-                  anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-                  targetOrigin={{horizontal: 'right', vertical: 'top'}}
-                >
-                  <MenuItem primaryText="Edit" onClick={this.handleEditReply}/>
-                  <MenuItem primaryText="Delete" onClick={this.handleDeleteReply} />
-                </IconMenu> : ''}
-              <br /> <br /> */}
-
               <div style={{
                 display: 'inline',
                 fontStyle: 'italic',
                 backgroundColor: yellow200,
               }}
               >
-                {(this.props.id === 0) ? this.props.articleText : ''}
+                {this.props.annotation.articleText}
               </div>
+              <a href={`${config.host}/discussion/${this.props.annotation.article}`}>{this.props.annotation.text}</a>
             </CardText>
           </Card>
         </MuiThemeProvider>
